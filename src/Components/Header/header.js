5810a20f8403c6d272c11081587f26ba80c78c88
jmraffi84 <jmraffi@gmail.com>
2023-04-16 12:54:20 +0530

    first commit

---
+++ src/Components/Header/header.js
@@ -0,0 +1,11 @@
+import './header.css'
+import SideBar from '../SideBar/sidebar';
+const Header=()=>{
+    return(
+        <div>
+            <SideBar />
+        </div>
+    )
+}
+
+export default Header;
